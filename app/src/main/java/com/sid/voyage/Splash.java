18c09118e7778c85cf99bd8757795841b7e4f46d
Siddharth saxena <siddharthsaxena0@gmail.com>
2017-04-08 14:22:51 +0530

    2O Clock commit - Just starter project

--- app/src/main/java/com/sid/voyage/Splash.java
+++ app/src/main/java/com/sid/voyage/Splash.java
@@ -1,7 +1,11 @@
 package com.sid.voyage;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TextView textView = (TextView)findViewById(R.id.title);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"code.otf");
+        textView.setTypeface(typeface);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+
+                startActivity(new Intent(Splash.this,MainActivity.class));
+
+                finish();
+
+            }
+        }, 9000);
     }
 }
---
+++ app/src/main/java/com/sid/voyage/Suggestion.java
@@ -0,0 +1,35 @@
+package com.sid.voyage;
+
+/**
+ * Created by sid on 08/04/17.
+ */
+
+public class Suggestion {
+
+    private String name, country, price, currency, cityId, image;
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+}
